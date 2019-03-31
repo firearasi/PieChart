643c206a703efc2b30e7f3e4b046dcab39251955
Cheng Yan <firearasi@qq.com>
2019-03-31 20:34:14 +0800

    windows management

--- main.py
+++ main.py
@@ -9,14 +9,16 @@ Created on Sat Mar 30 23:44:38 2019
 import sys
 from PySide2.QtCore import QCoreApplication
 from PySide2.QtWidgets import QApplication
-from PieChartWidget import PieChartWidget
+
 from PySide2.QtGui import QIcon
-        
+from MainWindow import MainWindow
 if __name__ == '__main__':
     print('main')
     app = QCoreApplication.instance()
     if app is None:
         app = QApplication(sys.argv)
     app.setWindowIcon(QIcon('pie2.png'))
-    ex = PieChartWidget()
+    ex = MainWindow()
+    app.setStyleSheet("QMainWindow(background-color: rgba(234, 255, 255, 12);}")
+    ex.setWindowTitle('PieCharts')
     sys.exit(app.exec_())
